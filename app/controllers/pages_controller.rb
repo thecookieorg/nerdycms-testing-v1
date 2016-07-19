99890d356d98310e63ed8c7931291e6d81cea42c
thecookieorg <marko@thecookie.org>
2016-07-18 19:16:49 -0700

    friendly_id added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -64,7 +64,7 @@ class PagesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_page
-      @page = Page.find(params[:id])
+      @page = Page.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
